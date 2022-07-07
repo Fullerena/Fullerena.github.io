94c4e696798a57f6ffae8f5f5cf99f651304af65
Haikal Isa <94767147+Fullerena@users.noreply.github.com>
2022-07-07 07:31:14 +0700

    Add files via upload

---
+++ PWA/sw.js
@@ -0,0 +1,24 @@
+const calculator = "calculator";
+const assets = [
+  "/",
+  "/calculator.html",
+  "/lit.css/lit.css",
+  "/lit.css/util.css",
+  "/alpine.min.js"
+  ]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(calculator).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
